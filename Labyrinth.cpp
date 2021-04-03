e706c72089c80bf444ca81bcb6bc1a2fe2acc5fa
Cheuk Lam Cheung <ccheung792423@gmail.com>
2021-04-03 10:25:42 -0400

    draft

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,8 +1,41 @@
 #include "Labyrinth.h"
+#include <iostream>
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
+    // /* TODO: Delete this comment and the remaining lines, then implement this function. */
+    // (void) start;
+    // (void) moves;
+    // return false;
+    int itemCount = 0;
+    for(int i=0; i<moves.length(); i++){
+        switch(moves[i]){
+            case 'N':
+                start = start->north;
+                break;
+            case 'E':
+                start = start->east;
+                break;
+            case 'S':
+                start = start->south;
+                break;
+            case 'W':
+                start = start->west;
+                break;
+            default:
+                break;
+        }
+        if (start == nullptr){
+            return false;
+        }
+        if (start->whatsHere != Item::NOTHING){
+            itemCount++;
+            start->whatsHere = Item::NOTHING;
+        }
+    }
+    if (itemCount == 3){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
--- Makefile
+++ Makefile
@@ -6,10 +6,12 @@ CXX=g++
 main: $(OBJECTS)
 	$(CXX) -g  -o main $(OBJECTS)
 Labyrinth.o: Labyrinth.cpp Labyrinth.h
+	g++ -std=c++11 -c Labyrinth.cpp
 
 Main.o: Main.cpp MazeGenerator.h Labyrinth.h
 
 MazeGenerator.o: MazeGenerator.cpp MazeGenerator.h Labyrinth.h
+	g++ -std=c++11 -c MazeGenerator.cpp
 
 
 clean:
